a3521a772f7d297b34fe8cabf10c8e116588f3fe
codeInvisibleInDark <bscs2312181@szabist.pk>
2025-05-14 10:57:04 +0000

    repair

--- Api/index.js
+++ Api/index.js
@@ -13,6 +13,27 @@ app.get('/',(req,res)=>{
         res.status(500).json({Error:err.message})
     }
 });
+
+
+
+app.get('/assign1', async (req, res) => {
+    try {
+        const result = await pool.query(`
+            SELECT 
+    (SELECT COUNT(*) FROM employees) AS emp_count,
+    (SELECT COUNT(*) FROM departments) AS dep_count,
+    (SELECT COUNT(*) FROM locations) AS loc_count,
+    (SELECT COUNT(*) FROM countries) AS con_count,
+    (SELECT COUNT(*) FROM regions) AS reg_count,
+    (SELECT COUNT(*) FROM jobs) AS job_count,
+    (SELECT COUNT(*) FROM job_history) AS count_jobhistory;
+
+        `);
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 app.get('/region',async(req,res)=>{
     try{
         const result=await pool.query('SELECT COUNT(*) FROM employees');
